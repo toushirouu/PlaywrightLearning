02748cf6771af7bdbf34f631ad056cf3bcf3c911
toushirouu <toushirouu@gmail.com>
2025-04-05 00:17:43 +0200

    Inherate from PageTest class and adjust the project

--- PlaywrightDemo/UnitTest1.cs
+++ PlaywrightDemo/UnitTest1.cs
@@ -4,39 +4,29 @@ using Microsoft.Playwright.NUnit;
 
 namespace PlaywrightDemo
 {
-    public class NunitPlaywright
+    public class NunitPlaywright : PageTest
     {
         [SetUp]
-        public void Setup()
+        public async Task Setup()
         {
+            await Page.GotoAsync(url: "http://www.eaapp.somee.com");
         }
 
         [Test]
         public async Task Test1()
         {
-            //Playwright
-            using var playwright = await Playwright.CreateAsync();
 
-            //Open the browser
-            await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
-            {
-                Headless = false
-            });
+           
+            await Page.ClickAsync(selector:"id=loginLink");
+            await Page.FillAsync(selector: "#UserName", value: "admin");
+            await Page.FillAsync(selector: "#Password", value: "password");
+            await Page.ClickAsync(selector: "#loginIn");
+            await Expect(Page.Locator(selector: "text=Employee Details")).ToBeVisibleAsync();
 
-            //Page
-            IPage page = await browser.NewPageAsync();
-            await page.GotoAsync(url: "http://www.eaapp.somee.com");
-            await page.ClickAsync(selector:"id=loginLink");
-            await page.ScreenshotAsync(new PageScreenshotOptions
+            await Page.ScreenshotAsync(new PageScreenshotOptions
             {
                 Path = "EaApp.jpg"
             });
-
-            await page.FillAsync(selector: "#UserName", value: "admin");
-            await page.FillAsync(selector: "#Password", value: "password");
-            await page.ClickAsync(selector: "#loginIn");
-            bool isExist = await page.Locator(selector: "text=Employee Details").IsVisibleAsync();
-            Assert.IsTrue(isExist);
 
         }
     }
