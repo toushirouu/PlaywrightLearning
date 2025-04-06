ae4d178d8ff573c234e256ce792dd39d35551d28
toushirouu <toushirouu@gmail.com>
2025-04-06 13:50:34 +0200

    Split tests to separted files

---
+++ PlaywrightDemo/NUnitPlaywright.cs
@@ -0,0 +1,40 @@
+using System.Runtime.InteropServices;
+using Microsoft.Playwright;
+using Microsoft.Playwright.NUnit;
+
+namespace PlaywrightDemo
+{
+    public class NunitPlaywright : PageTest
+    {
+        [SetUp]
+        public async Task Setup()
+        {
+            await Page.GotoAsync(url: "http://www.eaapp.somee.com");
+        }
+
+        [Test]
+        public async Task Test1()
+        {
+
+            var linkLogin = Page.Locator(selector: "#loginLink");
+            await linkLogin.ClickAsync();
+            await Page.FillAsync(selector: "#UserName", value: "admin");
+            await Page.FillAsync(selector: "#Password", value: "password");
+            await Page.ScreenshotAsync(new PageScreenshotOptions
+            {
+                Path = @"test\dane_do_logowania.jpg"
+            });
+
+            var btnLogin = Page.Locator(selector: "#loginIn", new PageLocatorOptions { HasTextString = "Log in" });
+            await btnLogin.ClickAsync();
+
+            await Expect(Page.Locator(selector: "text=Employee Details")).ToBeVisibleAsync();
+
+            await Page.ScreenshotAsync(new PageScreenshotOptions
+            {
+                Path = @"test\po_zalogowaniu.jpg"
+            });
+
+        }
+    }
+}
